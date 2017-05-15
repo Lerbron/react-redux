10913b22d0a83eb447152918bb48ed1dc35fa669
马齐玲 <maqiling01@chinatopcredit.com>
2017-05-15 14:08:54 +0800

    实现按需加载

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,14 @@ module.exports = {
   output: {
     path: __dirname + "/dist/",
     filename: "[name].js",
-    publicPath: "dist/"
+    publicPath: "dist/",
+	  chunkFilename: '[name].chunk.js'  // 添加按需加载的文件名
   },
   module: {
     loaders: [{
         test: /\.(jpg|png|gif)$/,
         exclude: /node_modules/,
-        loader: 'url-loader?limit=8192'
+	      use: 'url-loader?limit=8192'
       },{
         test: /\.(js|jsx)$/,
         use: [{
@@ -26,15 +27,15 @@ module.exports = {
       },{
         test: /\.css/,
         exclude: /node_modules/,
-        loader: 'style-loader!css-loader'
+        use: ['style-loader', 'css-loader']
       },{
         test: /\.scss$/,
         exclude: /node_modules/,
-        loader: 'style-loader!css-loader!sass-loader'
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },{
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, exclude: /node_modules/, loader: "url-loader?limit=10000&minetype=application/font-woff"
       },{
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, exclude: /node_modules/, loader: "file-loader"
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, exclude: /node_modules/, use: ["file-loader"]
       }]
   },
   resolve: {
