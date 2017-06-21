eaf1004a961bfb568212420aa4ac7db68aafe4fc
马齐玲 <maqiling01@chinatopcredit.com>
2017-06-21 14:22:15 +0800

    css 文件单独打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 var webpack = require("webpack");
 var path = require("path");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+/*
+*  css 单独打包：
+*   首先引入 ExtractTextPlugin 插件
+*   在module中使用 ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'})
+*   在plugins中通过new关键字进行实例化。new ExtractTextPlugin("[name].min.css")
+*
+*   注：在html页面中引入打包好的css文件才能有效
+* */
 // var UglifyJsPlugin = require("UglifyJsPlugin");
 
 module.exports = {
@@ -27,11 +37,13 @@ module.exports = {
       },{
         test: /\.css/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader']
+	      use: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'})
+        // use: ['style-loader', 'css-loader']
       },{
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+	      use: ExtractTextPlugin.extract({fallback: 'style-loader', use: ['css-loader', 'sass-loader']})
+        // use: ['style-loader', 'css-loader', 'sass-loader']
       },{
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, exclude: /node_modules/, loader: "url-loader?limit=10000&minetype=application/font-woff"
       },{
@@ -61,7 +73,8 @@ module.exports = {
       new webpack.LoaderOptionsPlugin({
         minimize: true
       }),
-      new webpack.optimize.CommonsChunkPlugin(['vendor'])
+      new webpack.optimize.CommonsChunkPlugin(['vendor']),
+	    new ExtractTextPlugin("[name].min.css")
     ],
   watch: true
 };
